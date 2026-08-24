0a6b84be3f9e62125239b190539d0a93bace4c9f
IrvinTovar <IrvinTovar@users.noreply.github.com>
2026-08-24 14:28:40 -0600

    Release 5.48.0

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,7 +31,7 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "PocketMine-Fenix";
-	public const BASE_VERSION = "5.47.0";
+	public const BASE_VERSION = "5.48.0";
 	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
 	public const GITHUB_URL = "https://github.com/PocketMine-Fenix/PocketMine-Fenix";
